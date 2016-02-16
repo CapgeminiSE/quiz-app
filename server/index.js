f6107ef29e44a16c7649848a7837b0501d826609
hogl <alexander.hoglander@gmail.com>
2016-02-16 15:17:27 +0100

    fix: indent

--- server/index.js
+++ server/index.js
@@ -2,21 +2,20 @@
 
 import express from 'express'
 import mongoose from 'mongoose'
-import {QuizQuestion} from '../models/quiz'
 const config = require('../config.json')
 const app = express()
 
 app.use(express.static('./public'))
 
 app.listen(config.PORT).on('listening', () => {
-    console.log('Listening on %s', config.PORT);
+  console.log('Listening on %s', config.PORT)
 })
 
 app.get('/', (request, response) => {
-    res.sendFile('./public/index.html')
+  response.sendFile('./public/index.html')
 })
 
 mongoose.connect(config.DB.URI, (err, db) => {
-    if (err) throw err
-    console.info('Connected to %s', config.DB.URI)
+  if (err) throw err
+  console.info('Connected to %s', config.DB.URI)
 })
