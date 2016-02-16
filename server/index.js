f29ca4dd648070f6deb8a8ab4ff390764db60293
hogl <alexander.hoglander@capgemini.se>
2016-02-16 14:24:45 +0100

    first commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import express from 'express'
+const config = require('../config.json')
+const app = express()
+
+app.listen(config.PORT).on('listening', () => {
+	console.log('Server listen on 8080');
+})
+
+app.get('*', (request, response) => {
+	response.send('Hello world');
+})
