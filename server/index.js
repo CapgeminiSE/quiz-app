90d2c5d1726a7d1eca669f7e04cb78a1a2d2d9a9
hogl <alexander.hoglander@gmail.com>
2016-02-16 14:33:53 +0100

    fix: tabs > spaces

--- server/index.js
+++ server/index.js
@@ -5,9 +5,9 @@ const config = require('../config.json')
 const app = express()
 
 app.listen(config.PORT).on('listening', () => {
-	console.log('Server listen on 8080');
+    console.log('Server listen on 8080');
 })
 
 app.get('*', (request, response) => {
-	response.send('Hello world');
+    response.send('Hello world');
 })
