6e09304b3c7451dd4d2f827d4cc7375e7a2d870c
hogl <alexander.hoglander@gmail.com>
2016-02-16 14:49:26 +0100

    add: mongoose

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 'use strict'
 
 import express from 'express'
+import mongoose from 'mongoose'
+import {QuizQuestion} from '../models/quiz'
 const config = require('../config.json')
 const app = express()
 
 app.listen(config.PORT).on('listening', () => {
-    console.log('Server listen on 8080');
+    console.log('Listening on %s', config.PORT);
 })
 
 app.get('*', (request, response) => {
     response.send('Hello world');
+})
+
+mongoose.connect(config.DB.URI, (err, db) => {
+    if (err) throw err
+    console.info('Connected to %s', config.DB.URI)
 })
