1290526a31ca4fe7117987315db145a0249901bf
hogl <alexander.hoglander@gmail.com>
2016-02-17 12:38:22 +0100

    add: JWT auth

--- server/index.js
+++ server/index.js
@@ -3,12 +3,21 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
+import jwt from 'jsonwebtoken'
 
 const config = require('../config.json')
 const app = express()
 
 app.use(bodyParser.json())
 app.use(express.static('./public'))
+
+let auth = (req, res, next) => {
+  jwt.verify(req.body.token, config.jwtSecret, (err, decoded) => {
+    if (err) return res.json({ok: false, message: 'Invalid token'})
+    req.decoded = decoded
+    next()
+  })
+}
 
 app.listen(config.PORT).on('listening', () => {
   console.log('Listening on %s', config.PORT)
@@ -19,8 +28,17 @@ app.get('/', (request, response) => {
 })
 
 app.post('/login', (req, res) => {
-  if (req.body.password === config.password) res.json({accepted: true})
-  else res.json({accepted: false})
+  if (req.body.password !== config.password) {
+    return res.json({accepted: false})
+  }
+
+  jwt.sign({test: 'test'}, config.jwtSecret, null, (token) => {
+    res.json({accepted: true, token})
+  })
+})
+
+app.post('/kontroll', auth, (req, res) => {
+  res.json({ok: true, decoded: req.decoded})
 })
 
 mongoose.connect(config.DB.URI, (err, db) => {
